95ec8afb79625eec0161555418b9c909ec2f54b2
sumpu#07 <samprada2058@gmail.com>
2025-03-21 21:05:03 +0545

    config variables for database,secret-key,algorithm,expire-time,upload-dir added

---
+++ config.py
@@ -0,0 +1,8 @@
+DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost/brainTumor"
+
+SECRET_KEY = "aserwaefwe1113"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+UPLOADS_FOLDER = "C:\\Users\\Administrator\\Documents\\brain_tumor_detection\\backend\\uploads"
+  # Change as needed
